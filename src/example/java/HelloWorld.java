4350f76e27f8f3cfc52132e795bab4017567894f
K4zoku <kazoku0356@gmail.com>
2022-01-25 01:33:08 +0700

    rename, rearrange code example

--- src/example/java/HelloWorld.java
+++ src/example/java/HelloWorld.java
@@ -1,8 +1,7 @@
-package com.github.k4zoku.kwrapper.lwjgl.example;
-
 import com.github.k4zoku.kwrapper.lwjgl.common.geometry.Size;
 import com.github.k4zoku.kwrapper.lwjgl.glfw.monitor.Monitor;
 import com.github.k4zoku.kwrapper.lwjgl.glfw.window.Window;
+import org.lwjgl.Version;
 import org.lwjgl.glfw.GLFWErrorCallback;
 import org.lwjgl.glfw.GLFWVidMode;
 import org.lwjgl.opengl.GL;
@@ -10,12 +9,56 @@ import org.lwjgl.opengl.GL;
 import static org.lwjgl.glfw.GLFW.*;
 import static org.lwjgl.opengl.GL11.*;
 
-public class LWJGLExample {
+public class HelloWorld {
 
     private Window window;
 
-    public static void main(String[] args) {
-        new LWJGLExample().run();
+    public void init() {
+        // Set up an error callback. The default implementation
+        // will print the error message in System.err.
+        GLFWErrorCallback.createPrint(System.err).set();
+
+        // Initialize GLFW. Most GLFW functions will not work before doing this.
+        if (!glfwInit()) {
+            throw new IllegalStateException("Unable to initialize GLFW");
+        }
+
+        // Configure GLFW
+        glfwDefaultWindowHints(); // optional, the current window hints are already the default
+        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE); // the window will stay hidden after creation
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE); // the window will be resizable
+
+        // Create window
+        window = new Window(300, 300, "Hello World!");
+
+        // Set up a key callback. It will be called every time a key is pressed, repeated or released.
+        window.setKeyCallback((key, scancode, action, mods) -> {
+            if (key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE) {
+                window.setShouldClose(true); // We will detect this in the rendering loop
+            }
+        });
+
+        // Get the resolution of the primary monitor
+        GLFWVidMode vidMode = Monitor.getPrimaryMonitor().getVideoMode();
+        assert vidMode != null;
+
+        // Get window size
+        Size<Integer> windowSize = window.getSize();
+
+        // Center the window
+        window.setPosition(
+                (vidMode.width() - windowSize.getWidth()) / 2,
+                (vidMode.height() - windowSize.getHeight()) / 2
+        );
+
+        // Make the OpenGL context current
+        window.makeContextCurrent();
+
+        // Enable v-sync
+        glfwSwapInterval(1);
+
+        // Make the window visible
+        window.show();
     }
 
     public void loop() {
@@ -43,54 +86,22 @@ public class LWJGLExample {
     }
 
     public void run() {
+        System.out.println("Hello LWJGL " + Version.getVersion() + "!");
+
         init();
         loop();
+
+        // Free the window callbacks and destroy the window
         window.freeCallbacks();
         window.destroy();
+
+        // Terminate GLFW and free the error callback
+        glfwTerminate();
+        glfwSetErrorCallback(null).free();
     }
 
-    public void init() {
-        // Set up an error callback. The default implementation
-        // will print the error message in System.err.
-        GLFWErrorCallback.createPrint(System.err).set();
-
-        // Initialize GLFW. Most GLFW functions will not work before doing this.
-        if (!glfwInit()) {
-            throw new IllegalStateException("Unable to initialize GLFW");
-        }
-
-        // Configure GLFW
-        glfwDefaultWindowHints(); // optional, the current window hints are already the default
-        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE); // the window will stay hidden after creation
-        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE); // the window will be resizable
-
-        // Create window
-        window = new Window(640, 480, "GLFW Test");
-
-        window.setKeyCallback((key, scancode, action, mods) -> {
-            if (key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE)
-                window.setShouldClose(true); // We will detect this in the rendering loop
-        });
-
-        // Get the resolution of the primary monitor
-        GLFWVidMode vidMode = Monitor.getPrimaryMonitor().getVideoMode();
-        assert vidMode != null;
-        Size<Integer> windowSize = window.getSize();
-
-        // Center the window
-        window.setPosition(
-                (vidMode.width() - windowSize.getWidth()) / 2,
-                (vidMode.height() - windowSize.getHeight()) / 2
-        );
-
-        // Make the OpenGL context current
-        window.makeContextCurrent();
-
-        // Enable v-sync
-        glfwSwapInterval(1);
-
-        // Make the window visible
-        window.show();
+    public static void main(String[] args) {
+        new HelloWorld().run();
     }
 
 }
